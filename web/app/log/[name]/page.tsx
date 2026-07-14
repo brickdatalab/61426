c6d1ea16c2e0bc8e69b9891bcf397cf1eff916ac
Cascano Group <actions@brickdata.io>
2026-07-14 09:54:36 -0400

    remove retired Vercel web app

--- web/app/log/[name]/page.tsx
+++
@@ -1,5 +0,0 @@
-import LogViewer from './LogViewer';
-
-export default function Page({ params }: { params: { name: string } }) {
-  return <LogViewer name={decodeURIComponent(params.name)} />;
-}
--- web/app/run/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import RunViewer from './RunViewer';
-
-export default function Page({ params }: { params: { id: string } }) {
-  return <RunViewer runId={params.id} />;
-}
